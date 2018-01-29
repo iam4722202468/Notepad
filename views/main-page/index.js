dbf1ff2a82362c8c66b1a936b2c557b80ca8c2d9
iam <iam4722202468@gmail.com>
2018-01-29 00:44:55 -0500

    Fixed a few spelling mistakes

--- views/main-page/index.js
+++ views/main-page/index.js
@@ -16,14 +16,14 @@ class MainPage extends React.Component {
         <hr/>
         <div className={style.moveRight}>
           <h5>Viewing Notes</h5>
-          <p>Notes can be viewed in the View a note tab. Each note has a name, ID, date created/edited and character length field, as well as buttons to edit, delete and download the note.</p>
-          <p>Notes can be downloaded in PDF format. This PDF contains the note name, time created/edited, and the note value (using a monospace font so ascii art looks cool). Clicking on the note name in the pdf will bring the user to a page which allows them to edit the note.</p>
+          <p>Notes can be viewed in the <b>View a note</b> tab. Each note has a name, ID, date created/edited and character length field, as well as buttons to edit, delete and download the note.</p>
+          <p>Notes can be downloaded in PDF format. This PDF contains the note name, time created/edited, and the note value (using a monospace font so ascii art looks cool). Clicking on the note name in the PDF will bring the user to a page which allows them to edit the note.</p>
           <hr/>
           <h5>Creating and Editing Notes</h5>
-          <p>Notes can be created in the create a note tab. Once a note has been created the name of it can't be edited, but the value of it can be. Every edit will result in an updated edit time on the note.</p>
+          <p>Notes can be created in the <b>Create a note</b> tab. Once a note has been created the name of it can't be edited, but the value of it can be. Every edit will result in an updated edit time on the note.</p>
           <p>When editing notes, the note can be viewed using markdown formatting.</p>
           <h5>Other Features</h5>
-          <p>This site uses SASS for CSS. It also looks nice on mobile devices</p>
+          <p>This site uses SASS for CSS. It also looks nice on mobile devices.</p>
           <hr/>
         </div>
         <i>Created by Alexander Parent for Focus21</i>
